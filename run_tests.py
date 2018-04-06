60514a1317673212f89096c97c92763ed2317729
izrik <izrik@izrik.com>
2018-04-06 12:00:04 -0500

    Check that it calls functions.

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-from wodehouse import eval_str
+from wodehouse import eval_str, create_default_state
 
 
 class WodehouseTest(unittest.TestCase):
@@ -14,6 +14,15 @@ class WodehouseTest(unittest.TestCase):
     def test_evals_variables(self):
         # when
         result = eval_str('abc', {'abc': 123})
+        # then
+        self.assertEqual(123, result)
+
+    def test_calls_functions(self):
+        # given
+        state = create_default_state()
+        state['onetwothree'] = lambda *args: 123
+        # when
+        result = eval_str('(onetwothree)', state)
         # then
         self.assertEqual(123, result)
 
