a5db02734ca8d0a9b3f7ea232862463ac19a96d7
izrik <izrik@izrik.com>
2019-01-07 15:16:23 -0600

    todo

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,8 @@ SOURCES=wodehouse,wtypes,functions,macros
 
 coverage run --branch --source=$SOURCES ./run_tests.py && \
     coverage run -a --branch --source=$SOURCES ./wodehouse.py tests/*tests.w && \
+# TODO: coverage run -a --branch --source=$SOURCES ./wodehouse.py wodehouse.w -- tests/*tests.w && \
+#   TODO: argparse
     coverage html && \
     flake8 *.py functions macros wtypes && \
     ./compare_sources.py
