ccaf6edcfc8ee057838c3beeb0d66ca6f31741db
izrik <izrik@izrik.com>
2019-01-08 13:28:58 -0600

    Clean up docstring.

--- macros/define.py
+++ macros/define.py
@@ -4,17 +4,9 @@ from wtypes.symbol import WSymbol
 
 
 class Define(WMagicMacro):
-    # TODO: complete re-do this class
     """
-    Every file will have a WScope object accessible only to that file. This
-    object will at first be empty. Every top-level expression in the file that
-    gets eval'd will have a new scope passed to it having the file-level scope
-    object as its immediate prototype. If, however, any `define` expressions
-    are eval'd, that will add an entry to the file-level scope. So, when we
-    call `(import "filename.w" name1 name2)`, that call will eval the entire
-    `filename.w` file as scoped before, and the resulting file-level scope
-    object will be returned. Then, the `name1` and `name2` from `filename.w`'s
-    scope will be added to the importing file's file-level scope.
+    Given a name and an expression, evaluate the expression and store it in
+    the enclosing scope under that name.
     """
 
     def call_magic_macro(self, exprs, scope):
