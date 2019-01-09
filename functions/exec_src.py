5e3f8d963aecaaf2b7d3a43175fa02695c952727
izrik <izrik@izrik.com>
2019-01-09 17:08:44 -0600

    Clean up imports.

--- functions/exec_src.py
+++ functions/exec_src.py
@@ -1,37 +1,12 @@
+
 from functions.eval import w_eval
-import functions.magic_function
 from functions.read import read_whitespace_and_comments, read_expr
-import functions.scope
-
-# import macros.apply
-# import macros.assert_
-# import macros.cond
-# import macros.define
-# import macros.if_
-# import macros.import_
-# import macros.lambda_
-# import macros.let
-# import wtypes.boolean
-# import wtypes.scope
-import wtypes.stream
-
-#
-# WMagicFunction = functions.magic_function.WMagicFunction
-# Apply = macros.apply.Apply
-# WAssert = macros.assert_.WAssert
-# Cond = macros.cond.Cond
-# Define = macros.define.Define
-# If = macros.if_.If
-# Import = macros.import_.Import
-# WLambda = macros.lambda_.WLambda
-# Let = macros.let.Let
-# WBoolean = wtypes.boolean.WBoolean
-# WScope = wtypes.scope.WScope
-WStream = wtypes.stream.WStream
+from functions.scope import create_module_scope
+from wtypes.stream import WStream
 
 
 def w_exec_src(src, enclosing_scope, filename=None):
-    ms = functions.scope.create_module_scope(
+    ms = create_module_scope(
         enclosing_scope=enclosing_scope, name=filename, filename=filename)
     stream = WStream(src, filename=filename)
     read_whitespace_and_comments(stream)
--- functions/scope.py
+++ functions/scope.py
@@ -1,51 +1,7 @@
-from functions.collections import w_map, w_in
-from functions.convert import int_from_str
-import functions.exec_src
-from functions.io import w_print, w_format, read_file
-from functions.lisp import car, cdr, cons, atom
-from functions.list import list_func
-from functions.logical import w_not, w_or, w_and, less_than, \
-    less_than_or_equal_to, greater_than, greater_than_or_equal_to, eq
-# from functions.magic_function import WMagicFunction
-from functions.math import add, sub, mult, div
-from functions.raise_ import w_raise
-from functions.str import w_str
-from functions.stream import stream, stream_has_chars, stream_get_next_char, \
-    stream_get_position, stream_peek
-from functions.symbol import symbol_at
-from functions.types import get_type, w_isinstance
-# from macros.define import Define
-# from macros.import_ import Import
+
 from wtypes.list import WList
 from wtypes.scope import WScope
-import functions.magic_function
-import functions.exception
 from wtypes.string import WString
-
-import macros.apply
-import macros.assert_
-import macros.cond
-import macros.define
-import macros.if_
-import macros.import_
-import macros.lambda_
-import macros.let
-import wtypes.boolean
-# import wtypes.scope
-import wtypes.stream
-
-WMagicFunction = functions.magic_function.WMagicFunction
-Apply = macros.apply.Apply
-WAssert = macros.assert_.WAssert
-Cond = macros.cond.Cond
-Define = macros.define.Define
-If = macros.if_.If
-Import = macros.import_.Import
-WLambda = macros.lambda_.WLambda
-Let = macros.let.Let
-WBoolean = wtypes.boolean.WBoolean
-# WScope = wtypes.scope.WScope
-WStream = wtypes.stream.WStream
 
 
 def new_scope(pairs=None):
@@ -115,6 +71,33 @@ def create_module_scope(enclosing_scope=None, name=None, filename=None):
 
 
 def create_global_scope():
+    from functions.collections import w_map, w_in
+    from functions.convert import int_from_str
+    from functions.exception import exception
+    from functions.exec_src import w_exec
+    from functions.io import w_print, w_format, read_file
+    from functions.lisp import car, cdr, cons, atom
+    from functions.list import list_func
+    from functions.logical import w_not, w_or, w_and, less_than, \
+        less_than_or_equal_to, greater_than, greater_than_or_equal_to, eq
+    from functions.magic_function import WMagicFunction
+    from functions.math import add, sub, mult, div
+    from functions.raise_ import w_raise
+    from functions.str import w_str
+    from functions.stream import stream, stream_has_chars, \
+        stream_get_next_char, stream_get_position, stream_peek
+    from functions.symbol import symbol_at
+    from functions.types import get_type, w_isinstance
+    from macros.apply import Apply
+    from macros.assert_ import WAssert
+    from macros.cond import Cond
+    from macros.define import Define
+    from macros.if_ import If
+    from macros.import_ import Import
+    from macros.lambda_ import WLambda
+    from macros.let import Let
+    from wtypes.boolean import WBoolean
+
     scope = WScope()
     scope.update({
         '+': WMagicFunction(add, scope, name='+'),
@@ -164,12 +147,11 @@ def create_global_scope():
         'get_position': WMagicFunction(stream_get_position, scope,
                                        name='get_position'),
         'peek': WMagicFunction(stream_peek, scope, name='peek'),
-        'exec': WMagicFunction(functions.exec_src.w_exec, scope, name='exec'),
+        'exec': WMagicFunction(w_exec, scope, name='exec'),
         'int_from_str': WMagicFunction(int_from_str, scope),
         'symbol_at': WMagicFunction(symbol_at, scope),
         'define': Define(),
         'import': Import(),
-        'exception': WMagicFunction(functions.exception.exception, scope,
-                                    check_args=False)
+        'exception': WMagicFunction(exception, scope, check_args=False)
     })
     return scope
