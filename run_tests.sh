f6c644c70722eccb40099a0312497277cf5860ca
izrik <izrik@izrik.com>
2019-01-09 23:09:28 -0600

    todo

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,7 @@ coverage run --branch --source=$SOURCES ./run_tests.py && \
     coverage run -a --branch --source=$SOURCES ./wodehouse.py tests/*tests.w && \
 # TODO: coverage run -a --branch --source=$SOURCES ./wodehouse.py wodehouse.w -- tests/*tests.w && \
 #   TODO: argparse
+#   TODO: unittest.w
     coverage html && \
     flake8 *.py functions macros wtypes && \
     ./compare_sources.py
