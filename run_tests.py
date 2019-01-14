738f7faca40427dc2e9f3bd4ecfd509fd82f0ef3
izrik <izrik@izrik.com>
2019-01-14 15:13:33 -0600

    Add some more tests for cond.

--- run_tests.py
+++ run_tests.py
@@ -418,6 +418,76 @@ class WodehouseTest(unittest.TestCase):
         # then
         self.assertEqual(WSymbol.get('b'), result)
 
+    def test_cond_condition_is_true_evaluates_true_side(self):
+        evaled = False
+
+        def f():
+            nonlocal evaled
+            evaled = True
+            return WSymbol.get('f')
+
+        scope = create_global_scope()
+        scope['f'] = WMagicFunction(f, scope)
+
+        # when
+        result = eval_str("(cond (true (f)))",
+                          scope)
+        # then
+        self.assertEqual(WSymbol.get('f'), result)
+        self.assertTrue(evaled)
+
+    def test_cond_condition_is_false_does_not_evaluate_prior_retvals(self):
+        evaled1 = False
+        evaled2 = False
+
+        def f1():
+            nonlocal evaled1
+            evaled1 = True
+            return WSymbol.get('f1')
+
+        def f2():
+            nonlocal evaled2
+            evaled2 = True
+            return WSymbol.get('f2')
+
+        scope = create_global_scope()
+        scope['f1'] = WMagicFunction(f1, scope)
+        scope['f2'] = WMagicFunction(f2, scope)
+
+        # when
+        result = eval_str("(cond (false (f1)) (true (f2)))",
+                          scope)
+        # then
+        self.assertEqual(WSymbol.get('f2'), result)
+        self.assertFalse(evaled1)
+        self.assertTrue(evaled2)
+
+    def test_cond_condition_is_true_does_not_evaluate_later_retvals(self):
+        evaled1 = False
+        evaled2 = False
+
+        def f1():
+            nonlocal evaled1
+            evaled1 = True
+            return WSymbol.get('f1')
+
+        def f2():
+            nonlocal evaled2
+            evaled2 = True
+            return WSymbol.get('f2')
+
+        scope = create_global_scope()
+        scope['f1'] = WMagicFunction(f1, scope)
+        scope['f2'] = WMagicFunction(f2, scope)
+
+        # when
+        result = eval_str("(cond (true (f1)) (false (f2)))",
+                          scope)
+        # then
+        self.assertEqual(WSymbol.get('f1'), result)
+        self.assertTrue(evaled1)
+        self.assertFalse(evaled2)
+
     def test_if_condition_is_true_returns_first_retval(self):
         # when
         result = eval_str("(if (< 2 3) 4 5)",
