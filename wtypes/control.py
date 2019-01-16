2db55237d640ac7a1fec87669160666f38974dcd
izrik <izrik@izrik.com>
2019-01-16 12:15:27 -0600

    docstring.

--- wtypes/control.py
+++ wtypes/control.py
@@ -3,8 +3,20 @@ from wtypes.object import WObject
 
 
 class WControl(WObject):
+    """
+    Object that tells w_eval what to do in special cases.
+
+    If exception is not None, then just return the whole control object.
+    If callback is not None, then evaluate expr and pass it to callback.
+        If scope is not None, use that when evaluating expr
+    Otherwise, simply return expr without evaluating it.
+
+    The stack attribute is used for informational purposes in other parts of
+    the program.
+    """
     def __init__(self, expr=None, scope=None, callback=None, exception=None,
                  stack=None):
+        """Initialize self."""
         super().__init__()
         self.expr = expr
         self.scope = scope
