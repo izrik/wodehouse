570177fdb3b6a678b471d07881337f08f32e539d
izrik <izrik@izrik.com>
2019-01-24 09:35:31 -0600

    todo

--- run_tests.sh
+++ run_tests.sh
@@ -5,9 +5,6 @@ SOURCES=wodehouse,wtypes,functions,macros
 coverage run --branch --source=$SOURCES -m unittest discover -s tests -p '*.py' -t . && \
     coverage run -a --branch --source=$SOURCES ./wodehouse.py --run-files tests/*tests.w && \
 # TODO: coverage run -a --branch --source=$SOURCES ./wodehouse.py wodehouse.w -- tests/*tests.w && \
-#   TODO: argparse
-#   TODO: unittest.w
-#   TODO: open files
     coverage html && \
     flake8 *.py functions macros wtypes && \
     ./compare_sources.py
