2cb530fb7b50c0618424821a993954a659599bc6
izrik <izrik@izrik.com>
2019-01-30 00:04:19 -0600

    Raise w-exceptions instead of py-exceptions.

--- macros/if_.py
+++ macros/if_.py
@@ -1,4 +1,5 @@
-from wtypes.control import WEvalRequired, WReturnValue
+from wtypes.control import WEvalRequired, WReturnValue, WRaisedException
+from wtypes.exception import WException
 from wtypes.magic_macro import WMagicMacro
 from wtypes.boolean import WBoolean
 from wtypes.scope import WScope
@@ -9,9 +10,9 @@ class If(WMagicMacro):
         if scope is None:
             scope = WScope()
         if len(exprs) not in [2, 3]:
-            raise Exception(
+            return WRaisedException(WException(
                 "Expected 2 or 3 arguments to if, got {} instead.".format(
-                    len(exprs)))
+                    len(exprs))))
         condition = exprs[0]
         true_retval = exprs[1]
         if len(exprs) > 2:
@@ -25,8 +26,8 @@ class If(WMagicMacro):
                 return WReturnValue(true_retval)
             if _cond_result is WBoolean.false:
                 return WReturnValue(false_retval)
-            raise Exception(
+            return WRaisedException(WException(
                 "Condition evaluated to a non-boolean value: "
-                "\"{}\" ({})".format(_cond_result, type(_cond_result)))
+                "\"{}\" ({})".format(_cond_result, type(_cond_result))))
 
         return WEvalRequired(expr=condition, callback=callback)
