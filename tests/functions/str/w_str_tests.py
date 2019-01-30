b820e5084e1af7227630fed80ed161990c881b86
izrik <izrik@izrik.com>
2019-01-30 16:20:51 -0600

    Test that things are stringified correctly.

--- tests/functions/str/w_str_tests.py
+++ tests/functions/str/w_str_tests.py
@@ -11,36 +11,48 @@ class StringTest(TestCase):
         # when
         result = eval_str("(str 123)", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("123", result)
 
     def test_str_strings_are_unchanged(self):
         # when
         result = eval_str("(str \"123\")", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("123", result)
 
     def test_str_stringifies_lists(self):
         # when
         result = eval_str("(str (list 1 2 3))", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("(1 2 3)", result)
 
     def test_str_stringifies_quoted_lists(self):
         # when
         result = eval_str("(str '(1 2 3))", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("(1 2 3)", result)
 
     def test_str_stringifies_symbols(self):
         # when
         result = eval_str("(str 'asdf)", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("asdf", result)
 
     def test_str_stringifies_quoted_symbols(self):
         # when
         result = eval_str("(str ''asdf)", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("'asdf", result)
 
     def test_str_stringifies_lambdas(self):
@@ -48,6 +60,8 @@ class StringTest(TestCase):
         result = eval_str("(str (lambda (x) (* x x)))",
                           create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("(lambda (x) (* x x))", result)
 
     def test_str_stringifies_magic_functions(self):
@@ -55,28 +69,37 @@ class StringTest(TestCase):
         result = eval_str("(str str)", create_builtins_module())
         # then
         self.assertIsInstance(result, WString)
-        self.assertTrue(result.value.startswith("str"))
+        self.assertIsInstance(result.value, str)
+        self.assertEqual("str", result)
 
     def test_str_stringifies_variables_values(self):
         # when
         result = eval_str("(let (a 123) (str a))", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("123", result)
 
     def test_str_stringifies_boolean_true(self):
         # when
         result = eval_str("(str true)", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("true", result)
 
     def test_str_stringifies_boolean_false(self):
         # when
         result = eval_str("(str false)", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("false", result)
 
     def test_str_stringifies_boolean_variable(self):
         # when
         result = eval_str("(let (a true) (str a))", create_builtins_module())
         # then
+        self.assertIsInstance(result, WString)
+        self.assertIsInstance(result.value, str)
         self.assertEqual("true", result)
