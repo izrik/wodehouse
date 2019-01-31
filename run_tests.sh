46867f465aeaa1e0a3fb0ae094f4af79bbc0cac7
izrik <izrik@izrik.com>
2019-01-30 23:48:42 -0600

    Add unittest.w to the main test script.

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ SOURCES=wodehouse,wtypes,functions,macros
 
 coverage run --branch --source=$SOURCES -m unittest discover -s tests -p '*.py' -t . && \
     coverage run -a --branch --source=$SOURCES ./wodehouse.py --run-files tests/*tests.w && \
-# TODO: coverage run -a --branch --source=$SOURCES ./wodehouse.py unittest.w -s tests -t . && \
+    coverage run -a --branch --source=$SOURCES ./wodehouse.py unittest.w -s tests && \
 # TODO: coverage run -a --branch --source=$SOURCES ./wodehouse.py wodehouse.w unittest.w -s tests -t . && \
     coverage html && \
     flake8 *.py functions macros wtypes modules tests && \
