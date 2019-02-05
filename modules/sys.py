5f088a38a1ef78d5ab815320e65436619b82c929
izrik <izrik@izrik.com>
2019-02-05 10:00:31 -0600

    Add the exit function.

--- modules/sys.py
+++ modules/sys.py
@@ -2,6 +2,7 @@ import sys
 from itertools import takewhile
 
 from wtypes.list import WList
+from wtypes.magic_function import WMagicFunction
 from wtypes.module import WModule
 
 
@@ -15,6 +16,7 @@ def create_sys_module(builtins_module, argv=None):
     argv = w_from_py(argv)
     mod = WModule(builtins_module=builtins_module, name='sys')
     mod['argv'] = argv
+    mod['exit'] = WMagicFunction(w_exit, mod, name='exit', check_args=False)
     return mod
 
 
@@ -28,3 +30,27 @@ def w_from_py(p):
     if isinstance(p, str):
         return WString(p)
     raise Exception(f'Unknown object type: {p} ({type(p)})')
+
+
+def py_from_w(w):
+    from wtypes.object import WObject
+    from wtypes.number import WNumber
+    from wtypes.scope import WScope
+    from functions.str import w_str
+    if not isinstance(w, WObject):
+        return w
+    if isinstance(w, WNumber):
+        return w.value
+    if isinstance(w, WList):
+        return [py_from_w(_) for _ in w]
+    if isinstance(w, WScope):
+        return w.dict
+    return w_str(w).value
+
+
+def w_exit(status=None):
+    # TODO: raise a SystemExit exception so finally hnadlers get triggered
+    #   TODO: filter exception handlers by type, so run-of-the-mill handlers
+    #         don't accidentally catch SystemExit
+    #   TODO: type system with sub-types and built-in exception classes
+    sys.exit(py_from_w(status))
