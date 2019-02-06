1a901c4c523c399f506a21527cd84b92a1b70f0e
izrik <izrik@izrik.com>
2019-02-06 16:14:02 -0600

    Compare Positions to each other.

--- wtypes/position.py
+++ wtypes/position.py
@@ -14,3 +14,19 @@ class Position(WObject):
 
     def get_source_line(self):
         return self.stream.get_line(self.line)
+
+    def __eq__(self, other):
+        if not isinstance(other, Position):
+            return False
+
+        def is_or_equals(a, b):
+            if a is None and b is None:
+                return True
+            if a is None or b is None:
+                return False
+            return a == b
+
+        return \
+            is_or_equals(self.filename, other.filename) and \
+            is_or_equals(self.line, other.line) and \
+            is_or_equals(self.char, other.char)
