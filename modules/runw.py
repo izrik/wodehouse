433d15b2cb497ebe862e39621a59ccd07da59792
izrik <izrik@izrik.com>
2020-10-12 17:16:06 -0500

    Add run_source to the runw module.

--- modules/runw.py
+++ modules/runw.py
@@ -5,10 +5,13 @@ from wtypes.module import WModule
 def create_runw_module(builtins_module):
     from wodehouse import run_module
     from wodehouse import run_file
+    from wodehouse import run_source
 
     module = WModule(name='runw', builtins_module=builtins_module)
     module['run_module'] = WMagicFunction(run_module, module,
                                           check_args=False)
     module['run_file'] = WMagicFunction(run_file, module, check_args=False)
+    module['run_source'] = WMagicFunction(run_source, module,
+                                          check_args=False)
 
     return module
