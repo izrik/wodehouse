55b1d06a05bfd743b9d3199deeba139f5f58fb2d
izrik <izrik@izrik.com>
2020-10-13 08:20:02 -0500

    Move the run_XXX functions to the Runtime class.

--- modules/runw.py
+++ modules/runw.py
@@ -3,15 +3,12 @@ from wtypes.module import WModule
 
 
 def create_runw_module(builtins_module, runtime):
-    from wodehouse import run_module
-    from wodehouse import run_file
-    from wodehouse import run_source
-
     module = WModule(name='runw', builtins_module=builtins_module)
-    module['run_module'] = WMagicFunction(run_module, module,
+    module['run_module'] = WMagicFunction(runtime.run_module, module,
                                           check_args=False)
-    module['run_file'] = WMagicFunction(run_file, module, check_args=False)
-    module['run_source'] = WMagicFunction(run_source, module,
+    module['run_file'] = WMagicFunction(runtime.run_file, module,
+                                        check_args=False)
+    module['run_source'] = WMagicFunction(runtime.run_source, module,
                                           check_args=False)
 
     return module
