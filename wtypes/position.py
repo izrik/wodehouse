df8d29ba22def0ba1367d2fe6f53b40aafc81c71
izrik <izrik@izrik.com>
2021-04-14 13:36:25 -0500

    Do a better check of the inputs' types.

--- wtypes/position.py
+++ wtypes/position.py
@@ -3,14 +3,69 @@ from wtypes.object import WObject
 
 class Position(WObject):
     def __init__(self, filename, line, char, stream):
+        from functions.types import get_type
+        from wtypes.string import WString
+        from wtypes.number import WNumber
+        from wtypes.exception import WException, WrappedWException
+        from wtypes.stream import WStream
+
+        super().__init__()
+
+        if filename is None:
+            filename = WString('')
+        if isinstance(filename, str):
+            filename = WString(filename)
+        if not isinstance(filename, WObject):
+            raise TypeError(f'Argument "filename" must be WString. '
+                            f'Got "{filename}" ({type(filename)}) instead.')
+        if not isinstance(filename, WString):
+            raise WrappedWException(
+                WException(f'Argument "filename" must be String. '
+                           f'Got "{filename}" ({get_type(filename)}) '
+                           f'instead.'))
+
+        if isinstance(line, int):
+            line = WNumber(line)
+        if not isinstance(line, WObject):
+            raise TypeError(f'Argument "line" must be WNumber. '
+                            f'Got "{line}" ({type(line)}) instead.')
+        if not isinstance(line, WNumber):
+            raise WrappedWException(
+                WException(f'Argument "line" must be Number. '
+                           f'Got "{line}" ({get_type(line)}) instead.'))
+
+        if isinstance(char, int):
+            char = WNumber(char)
+        if not isinstance(char, WObject):
+            raise TypeError(f'Argument "char" must be WNumber. '
+                            f'Got "{char}" ({type(char)}) instead.')
+        if not isinstance(char, WNumber):
+            raise WrappedWException(
+                WException(f'Argument "char" must be Number. '
+                           f'Got "{char}" ({get_type(char)}) instead.'))
+
+        if not isinstance(stream, WObject):
+            raise TypeError(f'Argument "stream" must be WStream. '
+                            f'Got "{stream}" ({type(stream)}) instead.')
+        if not isinstance(stream, WStream):
+            raise WrappedWException(
+                WException(f'Argument "stream" must be Stream. '
+                           f'Got "{stream}" ({get_type(stream)}) instead.'))
+
         self.filename = filename
         self.line = line
         self.char = char
         self.stream = stream
 
     def __str__(self):
-        filename = self.filename or '<unknown>'
-        return f'{filename}:{self.line},{self.char}'
+        filename = self.filename.value or '<unknown>'
+        line = self.line
+        if line is None:
+            line = '<unknown>'
+        char = self.char
+        if char is None:
+            char = '<unknown>'
+        return f'{filename}:{line},{char}'
 
     def get_source_line(self):
         return self.stream.get_line(self.line)
