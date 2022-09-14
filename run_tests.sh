05e9d5ed5f664983c08f6fe57e9807a40e5aeb65
izrik <izrik@izrik.com>
2022-09-13 20:46:26 -0500

    Switch test runners.

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 SOURCES=wodehouse,wtypes,functions,macros,modules,runtime,version
 
 echo "Running python unit tests in tests/ with coverage..." && \
-    coverage run --branch --source=$SOURCES -m unittest discover -s tests -p '*.py' -t . && \
+    coverage run --branch --source=$SOURCES -m pytest -o python_files=\*.py tests/ && \
     echo "" && echo "Running w-lang unit tests in tests/ with w-coverage via wodehouse.py with py-coverage..." && \
     coverage run -a --branch --source=$SOURCES ./wodehouse.py -m coverage run -m unittest -s tests && \
 # TODO: coverage run -a --branch --source=$SOURCES ./wodehouse.py wodehouse.w unittest.w -s tests -t . && \
